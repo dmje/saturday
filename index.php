e31e220ea68f3c9f5f38af71ab66a6a26109a578
Mike Ellis <mike@thirty8.co.uk>
2026-02-07 20:47:33 +0000

    Colour tweak

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ foreach ($songs as $song) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Final Trajectory</title>
-    <link rel="stylesheet" href="assets/css/style.css?v3">
+    <link rel="stylesheet" href="assets/css/style.css?v4">
     <script defer src="https://simplestats.thirty8.co.uk/script.js" data-website-id="431dec11-3871-416f-90f1-fa919989d3e5"></script>
 </head>
 <body>
