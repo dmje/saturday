7ea92ec58e7d52412f513d48e53b21c9dc2563ed
Mike Ellis <mike@thirty8.co.uk>
2026-02-07 20:37:22 +0000

    Re-add Umami

--- 13-31.php
+++ 13-31.php
@@ -39,6 +39,7 @@ function isVideo($filename) {
     <title><?php echo $currentSong;?> - The Final Trajectory</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/<?php echo $currentSong;?>.css">
+    <script defer src="https://simplestats.thirty8.co.uk/script.js" data-website-id="431dec11-3871-416f-90f1-fa919989d3e5"></script>
 </head>
 <body class="song-page">
     <canvas id="bifurcation-bg"></canvas>
